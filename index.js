5b8ba51adb1e478a97737607df47f755c5ab6ed1
colin-maurier <maurierc@oregonstate.edu>
2022-10-23 19:58:44 -0400

    commit 3

--- index.js
+++ index.js
@@ -2,24 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 const axios = require('axios')
+var zip = 48843;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/data', (req, res) => {
     res.send('data from zip')
-    axios.get("https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=48843&key=AIzaSyC_2DWglQ07_Bbnc_p4GeWClyID9ZgDDIQ")
+    axios.get(`https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=${zip}&key=AIzaSyC_2DWglQ07_Bbnc_p4GeWClyID9ZgDDIQ`)
     .then(response => {
+        //log data in console
         console.log(response.data);
     })
+    //check for error
     .catch(err => {
         console.error(err);
+        //show status code if there is error
+        res.sendStatus(500);
     })
     res.send("done.");
-    
+    //show status
+    res.sendStatus(200);
   })
-
+//listen port
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Local Politician app listening on port ${port}`)
 })
