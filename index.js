c13eb25d16dca1f1b6273e7aed589291c8c7789d
colin-maurier <maurierc@oregonstate.edu>
2022-10-23 19:18:50 -0400

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+const express = require('express')
+const app = express()
+const port = 3000
+const axios = require('axios')
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/data', (req, res) => {
+    res.send('data from zip')
+    axios.get("https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=48843&key=AIzaSyC_2DWglQ07_Bbnc_p4GeWClyID9ZgDDIQ")
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(err => {
+        console.error(err);
+    })
+    res.send("done.");
+    
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
